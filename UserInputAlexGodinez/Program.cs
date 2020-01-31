bc7b9a5d4de3a18240553aa2a951507fc27247bd
godin <godin@DESKTOP-OI7FTBA>
2020-01-31 18:28:14 -0500

    Add project files.

---
+++ UserInputAlexGodinez/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UserInputAlexGodinez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, This is Alex Godinez");
+            Console.WriteLine("Type your first and last name, when you are done, hit enter.");
+            string name = Console.ReadLine();
+            Console.WriteLine("Good Job,You typed: " + name);
+
+        }
+    }
+}
